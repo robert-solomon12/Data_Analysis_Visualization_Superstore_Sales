e183722723526338cd618cdb80902d8ec1c33f3a
Robert Solomon <robertsss23@outlook.com>
2024-07-26 00:39:07 +0100

    Added remaining column names and data types appropriate and based on common attributes found in sales datasets

--- src/Controller_SQL/salesDB.sql
+++ src/Controller_SQL/salesDB.sql
@@ -4,25 +4,32 @@ DROP DATABASE IF EXISTS salesData;
 CREATE DATABASE IF NOT EXISTS salesData;
 USE salesData;
 
-CREATE TABLE sales (
+CREATE TABLE IF NOT EXISTS sales (
     OrderID INT PRIMARY KEY,
     OrderDate DATE,
     ShipDate DATE,
-    CustomerID INT,
+    ShipMode VARCHAR(255),
+    CustomerID VARCHAR(255),
     CustomerName VARCHAR(255),
-    ProductID INT,
+    Segment VARCHAR(255),
+    Country VARCHAR(255),
+    City VARCHAR(255),
+    State VARCHAR(255),
+    PostalCode VARCHAR(255),
+    Region VARCHAR(255),
+    ProductID VARCHAR(255),
+    Category VARCHAR(255),
+    SubCategory VARCHAR(255),
     ProductName VARCHAR(255),
     Sales DECIMAL(10, 2),
     Quantity INT,
     Discount DECIMAL(5, 2),
-    Profit DECIMAL(10, 2),
-    Region VARCHAR(255)
+    Profit DECIMAL(10, 2)
 );
 
 SHOW VARIABLES LIKE 'secure_file_priv';
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/superstore_Data.csv'
-INTO TABLE sales
+LOAD DATA INFILE 'C:/ProgramData/MySQL/superstore_Data.csv' INTO TABLE sales
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
